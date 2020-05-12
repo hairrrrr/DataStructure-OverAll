cf60462c6977d3248e22a7a5deb51e420f168471
hairrrrr <781728963@qq.com>
2020-05-12 11:10:56 +0800

    5-12

---
+++ C 数据结构/02 栈/01 顺序表实现/stack.c
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//顺序表实现栈
+
+#include <stdlib.h>
+#include <stdio.h>
+#include<stdbool.h>
+
+typedef int DataType;
+
+typedef struct Stack
+{
+	DataType* array;
+	size_t size;
+	size_t capacity;
+}Stack;
+
+void stackInit(Stack* s, size_t n);
+
+void stackPush(Stack* s, DataType data);
+
+DataType stackPop(Stack* s);
+
+size_t stackSize(Stack* s);
+
+bool stackIsEmpty(Stack* s);
+
+bool stackIsFull(Stack* s);
+
+void stackDestory(Stack* s);
+
+int main(void) {	
+
+	Stack s;
+
+	stackInit(&s, 2);
+
+	stackPush(&s, 1);
+	stackPush(&s, 2);
+	stackPush(&s, 3);
+	stackPush(&s, 4);
+
+	printf("%d\n", stackPop(&s));
+	printf("%d\n", stackPop(&s));
+	
+
+
+
+	return 0;
+}
+
+void stackInit(Stack* s, size_t n) {
+
+	s->array = (DataType*)malloc(sizeof(DataType) * n);
+
+	if (s->array == NULL) {
+		printf("malloc failed in %s\n", __func__);
+		exit(EXIT_FAILURE);
+	}
+
+	s->size = 0;
+	s->capacity = n;
+}
+
+void stackPush(Stack* s, DataType data) {
+	
+	//if (stackIsFull(s) == true) {
+	//	printf("push failed in %s, stack is full\n", __func__);
+	//	exit(EXIT_FAILURE);
+	//}
+
+	// 提供错误消息是可行的，但是如果提供了解决方法可能会让用户体验更好，我们可以在栈满的时候提供扩容
+	if (stackIsFull(s) == true) {
+		s->capacity *= 2;
+		s->array = (DataType*)realloc(s->array, s->capacity * sizeof(DataType));
+	}
+
+	s->array[s->size++] = data;
+}
+
+DataType stackPop(Stack* s) {
+
+	if (stackIsEmpty(s) == true) {
+		printf("pop failed in %s, stack is empty\n", __func__);
+		exit(EXIT_FAILURE);
+	}
+
+	return s->array[--s->size];
+}
+
+size_t stackSize(Stack* s) {
+
+	return s->size;
+}
+
+bool stackIsEmpty(Stack* s) {
+	if (s->size == 0)
+		return true;
+	return false;
+}
+
+bool stackIsFull(Stack* s) {
+	if (s->size == s->size)
+		return true;
+	return false;
+}
+
+void stackDestory(Stack* s) {
+
+	free(s->array);
+	s->array = NULL;
+	s->capacity = s->size = 0;
+}
---
+++ C 数据结构/03 队列/queue.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef int DataType;
+
+typedef struct Node
+{
+	struct Node* next;
+	DataType data;
+}Node;
+
+typedef struct Queue
+{
+	Node* front;
+	Node* rear;
+	int size;
+}Queue;
+
+void queueInit(Queue* q);
+// 创建队列结点
+Node* creatNode(DataType data);
+// 入队
+void queuePush(Queue* q, DataType data);
+// 出队
+DataType queuePop(Queue* q);
+// 判空
+bool queueIsEmpty(Queue* q);
+// 销毁
+void queueDestory(Queue* q);
+
+int main(void) {
+
+	Queue q;
+
+	queueInit(&q);
+
+	queuePush(&q, 1);
+	queuePush(&q, 2);
+	
+	printf("%d\n", queuePop(&q));
+	printf("%d\n", queuePop(&q));
+
+	return 0;
+}
+
+void queueInit(Queue* q) {
+
+	// 初始化空队列
+	q->front = q->rear = NULL;
+	q->size = 0;
+}
+
+
+Node* creatNode(DataType data) {
+
+	Node* new_node = (Node*)malloc(sizeof(Node));
+
+	if (new_node == NULL) {
+		printf("malloc failed in %s\n", __func__);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->data = data;
+	new_node->next = NULL;
+
+	return new_node;
+}
+
+
+void queuePush(Queue* q, DataType data) {
+	
+	Node* new_node = creatNode(data);
+
+	if (queueIsEmpty(q) == true) {
+		q->front = q->rear = new_node;
+	}
+	else {
+		q->rear->next = new_node;
+		q->rear = new_node;
+	}
+	q->size++;
+}
+
+
+DataType queuePop(Queue* q) {
+
+	DataType ret;
+	Node* ele_pop;
+
+	if (queueIsEmpty(q) == true) {
+		printf("pop failed in %s, the queue is empty\n", __func__);
+		exit(EXIT_FAILURE);
+	}
+
+	ele_pop = q->front;
+	ret = ele_pop->data;
+	q->front = ele_pop->next;
+	free(ele_pop);
+
+	if (queueIsEmpty(q) == true) {
+		q->rear = NULL;
+	}
+
+	q->size--;
+
+	return ret;
+}
+
+
+bool queueIsEmpty(Queue* q) {
+
+	if (q->front == NULL)
+		return true;
+	return false;
+}
+
+
+void queueDestory(Queue* q) {
+
+	Node* cur = q->front;
+
+	for (; cur != NULL; cur = q->front) {
+		q->front = cur->next;
+		free(cur);
+	}
+
+	q->front = q->rear = NULL;
+	q->size == 0;
+}
