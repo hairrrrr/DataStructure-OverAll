48d7ff61eb06f498ccf872cf2861df12a631bd0b
superhandsomechuan <781728963@qq.com>
2020-06-09 09:10:13 +0800

    1

--- C 数据结构/02 栈/01 顺序表实现/stack.c
+++ C 数据结构/02 栈/01 顺序表实现/stack.c
@@ -4,30 +4,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include<stdbool.h>
 
-typedef int DataType;
-
-typedef struct Stack
-{
-	DataType* array;
-	size_t size;
-	size_t capacity;
-}Stack;
-
-void stackInit(Stack* s, size_t n);
-
-void stackPush(Stack* s, DataType data);
-
-DataType stackPop(Stack* s);
-
-size_t stackSize(Stack* s);
-
-bool stackIsEmpty(Stack* s);
-
-bool stackIsFull(Stack* s);
-
-void stackDestory(Stack* s);
 
 int main(void) {	
 
